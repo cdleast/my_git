ea3fc6e46463ec8a684fa7420408df34e9062546
chendongliang <747439590@qq.com>
2020-04-24 10:27:52 +0800

    验证规则抽取

--- new-oa/src/components/proces/danganguanli/hetongzhulu.vue
+++ new-oa/src/components/proces/danganguanli/hetongzhulu.vue
@@ -5,86 +5,92 @@
             <p>北京首创股份有限公司</p>
             <p>{{$router.currentRoute.meta.title}}</p>
         </div>
-        <el-form class="child-form" ref="form" :model="form" label-width="100px">
+        <el-form
+            class="ruleForm"
+            ref="ruleForm"
+            :model="ruleForm"
+            :rules="rules"
+            label-width="100px"
+        >
             <el-row :gutter="20">
                 <el-col :span="12">
-                    <el-form-item label="全宗号">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="全宗号" prop="FONDS_ID">
+                        <el-input v-model="ruleForm.FONDS_ID"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="全宗名">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="全宗名" prop="FONDS_NAME">
+                        <el-input v-model="ruleForm.FONDS_NAME"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="档号">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="档号" prop="DA_CODE">
+                        <el-input v-model="ruleForm.DA_CODE"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="年度">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="年度" prop="DA_YEAR">
+                        <el-input v-model="ruleForm.DA_YEAR"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="归档部门">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="归档部门" prop="DEPT_NAME">
+                        <el-input v-model="ruleForm.DEPT_NAME"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="保管期限">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="保管期限" prop="DA_TERM">
+                        <el-input v-model="ruleForm.DA_TERM"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="密级">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="密级" prop="ARCH_SECRET">
+                        <el-input v-model="ruleForm.ARCH_SECRET"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="是否公开">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="是否公开" prop="CT_STIME">
+                        <el-input v-model="ruleForm.CT_STIME"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="24">
-                    <el-form-item label="题名">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="题名" prop="ARCH_TITLE">
+                        <el-input v-model="ruleForm.ARCH_TITLE"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="合同相关方">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="合同相关方" prop="CT_UNIT">
+                        <el-input v-model="ruleForm.CT_UNIT"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="所属项目">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="所属项目" prop="CT_PROJ">
+                        <el-input v-model="ruleForm.CT_PROJ"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="责任者">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="责任者" prop="DUTY_NAME">
+                        <el-input v-model="ruleForm.DUTY_NAME"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="档案目录">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="档案目录" prop="ID_NAME">
+                        <el-input v-model="ruleForm.ID_NAME"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="盒号">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="盒号" prop="BOX_NUM">
+                        <el-input v-model="ruleForm.BOX_NUM"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="签订时间">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="签订时间" prop="FONDS_ID">
+                        <el-input v-model="ruleForm.FONDS_ID"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="合同总金额">
-                        <el-input v-model="form.name"></el-input>
+                    <el-form-item label="合同总金额" prop="CT_EMONEY">
+                        <el-input v-model="ruleForm.CT_EMONEY"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
@@ -92,12 +98,14 @@
 
         <div class="upload-some">
             <div class="upload-header">原文信息</div>
+            
         </div>
     </div>
 </template>
 
 <script>
 import procesChildBarnav from "@/views/proces/proces-child-barnav";
+import rules from "@/utils/rules/index";
 export default {
     name: "hetongzhulu",
     components: {
@@ -105,9 +113,25 @@ export default {
     },
     data() {
         return {
-            form: {
-                name: ""
-            }
+            ruleForm: {
+                FONDSID: "", // 全宗号
+                FONDS_NAME: "", // 全宗名
+                DA_CODE: "", // 档号
+                DA_YEAR: "", // 年度
+                DEPT_NAME: "", // 归档部门
+                DA_TERM: "", // 保管期限
+                ARCH_SECRET: "", // 密级
+                IS_PUBLIC: "", // 是否公开
+                ARCH_TITLE: "", // 题名
+                CT_UNIT: "", // 合同相关方
+                CT_PROJ: "", // 所属项目
+                DUTY_NAME: "", // 责任者
+                ID_NAME: "", // 档案目录
+                BOX_NUM: "", // 盒号
+                CT_STIME: "", // 签订时间
+                CT_EMONEY: "" // 合同总金额
+            },
+            rules
         };
     }
 };
@@ -127,7 +151,7 @@ export default {
     border-bottom: 1px dashed #409eff;
 }
 
-.child-form {
+.ruleForm {
     padding: 30px 20px 0;
     background: #fff;
     margin-bottom: 20px;
--- new-oa/src/utils/rules/index.js
+++ new-oa/src/utils/rules/index.js
@@ -1,10 +1,53 @@
-export default{
-    rules: {
-        username: [
-            { required: true, message: "帐号不能为空", trigger: "blur" }
-        ],
-        password: [
-            { required: true, message: "密码不能为空", trigger: "blur" }
-        ]
-    }
+const rules = {
+    // 合同著录
+    FONDS_ID: [
+        { required: true, message: "全宗号不能为空", trigger: "blur" }
+    ],
+    FONDS_NAME: [
+        { required: true, message: "全宗名不能为空", trigger: "blur" }
+    ],
+    DA_CODE: [
+        { required: true, message: "档号不能为空", trigger: "blur" }
+    ],
+    DA_YEAR: [
+        { required: true, message: "年度不能为空", trigger: "blur" }
+    ],
+    DEPT_NAME: [
+        { required: true, message: "归档部门不能为空", trigger: "blur" }
+    ],
+    DA_TERM: [
+        { required: true, message: "保管期限不能为空", trigger: "blur" }
+    ],
+    ARCH_SECRET: [
+        { required: true, message: "密级不能为空", trigger: "blur" }
+    ],
+    CT_STIME: [
+        { required: true, message: "是否公开不能为空", trigger: "blur" }
+    ],
+    ARCH_TITLE: [
+        { required: true, message: "题名不能为空", trigger: "blur" }
+    ],
+    CT_UNIT: [
+        { required: true, message: "合同相关方不能为空", trigger: "blur" }
+    ],
+    CT_PROJ: [
+        { required: true, message: "所属项目不能为空", trigger: "blur" }
+    ],
+    DUTY_NAME: [
+        { required: true, message: "责任者不能为空", trigger: "blur" }
+    ],
+    ID_NAME: [
+        { required: true, message: "档案目录不能为空", trigger: "blur" }
+    ],
+    BOX_NUM: [
+        { required: true, message: "盒号不能为空", trigger: "blur" }
+    ],
+    FONDS_ID: [
+        { required: true, message: "签订时间不能为空", trigger: "blur" }
+    ],
+    CT_EMONEY: [
+        { required: true, message: "合同总金额不能为空", trigger: "blur" }
+    ]
 }
+
+export default rules
