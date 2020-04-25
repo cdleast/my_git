08e70ea589fcf507bd81a2609eca967604f9e9a9
chendongliang <747439590@qq.com>
2020-04-25 17:19:26 +0800

    后台管理模板

--- new-oa/src/components/proces/danganguanli/hetongzhulu.vue
+++ new-oa/src/components/proces/danganguanli/hetongzhulu.vue
@@ -14,83 +14,125 @@
         >
             <el-row :gutter="20">
                 <el-col :span="12">
-                    <el-form-item label="全宗号" prop="FONDS_ID">
-                        <el-input v-model="ruleForm.FONDS_ID"></el-input>
+                    <el-form-item label="全宗号" prop="HEZL_QZH">
+                        <el-input clearable :disabled="true" v-model="ruleForm.HEZL_QZH"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="全宗名" prop="FONDS_NAME">
-                        <el-input v-model="ruleForm.FONDS_NAME"></el-input>
+                    <el-form-item label="全宗名" prop="HEZL_QZM">
+                        <el-input clearable :disabled="true" v-model="ruleForm.HEZL_QZM"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="档号" prop="DA_CODE">
-                        <el-input v-model="ruleForm.DA_CODE"></el-input>
+                    <el-form-item label="档号" prop="HEZL_DH">
+                        <el-input clearable :disabled="true" v-model="ruleForm.HEZL_DH"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="年度" prop="DA_YEAR">
-                        <el-input v-model="ruleForm.DA_YEAR"></el-input>
+                    <el-form-item label="年度" prop="HEZL_ND">
+                        <el-select v-model="ruleForm.HEZL_ND" placeholder="选择年份">
+                            <el-option
+                                v-for="item in NDOPT"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value"
+                            ></el-option>
+                        </el-select>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="归档部门" prop="DEPT_NAME">
-                        <el-input v-model="ruleForm.DEPT_NAME"></el-input>
+                    <el-form-item label="归档部门" prop="HEZL_GDBM">
+                        <el-input
+                            readonly="readonly"
+                            @click.native="dialog_GDBM = true"
+                            clearable
+                            v-model="ruleForm.HEZL_GDBM"
+                            class="onColor"
+                        ></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="保管期限" prop="DA_TERM">
-                        <el-input v-model="ruleForm.DA_TERM"></el-input>
+                    <el-form-item label="保管期限" prop="HEZL_BGQX">
+                        <el-select v-model="ruleForm.HEZL_BGQX" placeholder="选择期限">
+                            <el-option
+                                v-for="item in BGQXOPT"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value"
+                            ></el-option>
+                        </el-select>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="密级" prop="ARCH_SECRET">
-                        <el-input v-model="ruleForm.ARCH_SECRET"></el-input>
+                    <el-form-item label="密级" prop="HEZL_MJ">
+                        <el-select v-model="ruleForm.HEZL_MJ" placeholder="选择密级">
+                            <el-option
+                                v-for="item in MKOPT"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value"
+                            ></el-option>
+                        </el-select>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="是否公开" prop="CT_STIME">
-                        <el-input v-model="ruleForm.CT_STIME"></el-input>
+                    <el-form-item label="是否公开" prop="HEZL_SFGK">
+                        <el-radio-group class="radioBorder" v-model="ruleForm.HEZL_SFGK">
+                            <el-radio :label="1">是</el-radio>
+                            <el-radio :label="2">否</el-radio>
+                        </el-radio-group>
                     </el-form-item>
                 </el-col>
                 <el-col :span="24">
-                    <el-form-item label="题名" prop="ARCH_TITLE">
-                        <el-input v-model="ruleForm.ARCH_TITLE"></el-input>
+                    <el-form-item label="题名" prop="HEZL_TM">
+                        <el-input clearable v-model="ruleForm.HEZL_TM"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="合同相关方" prop="CT_UNIT">
-                        <el-input v-model="ruleForm.CT_UNIT"></el-input>
+                    <el-form-item label="合同相关方" prop="HEZL_HTXGF">
+                        <el-input clearable v-model="ruleForm.HEZL_HTXGF"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="所属项目" prop="CT_PROJ">
-                        <el-input v-model="ruleForm.CT_PROJ"></el-input>
+                    <el-form-item label="所属项目" prop="HEZL_SSXM">
+                        <el-input clearable v-model="ruleForm.HEZL_SSXM"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="责任者" prop="DUTY_NAME">
-                        <el-input v-model="ruleForm.DUTY_NAME"></el-input>
+                    <el-form-item label="责任者" prop="HEZL_ZEZ">
+                        <el-input
+                            readonly="readonly"
+                            @click.native="dialog_ZRZ = true"
+                            clearable
+                            v-model="ruleForm.HEZL_ZRZ"
+                            class="onColor"
+                        ></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="档案目录" prop="ID_NAME">
-                        <el-input v-model="ruleForm.ID_NAME"></el-input>
+                    <el-form-item label="档案目录" prop="HEZL_DNML">
+                        <el-input
+                            readonly="readonly"
+                            @click.native="dialog_DNML = true"
+                            clearable
+                            v-model="ruleForm.HEZL_DNML"
+                            class="onColor"
+                        ></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="盒号" prop="BOX_NUM">
-                        <el-input v-model="ruleForm.BOX_NUM"></el-input>
+                    <el-form-item label="盒号" prop="HEZL_HH">
+                        <el-input clearable v-model="ruleForm.HEZL_HH"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="签订时间" prop="FONDS_ID">
-                        <el-input v-model="ruleForm.FONDS_ID"></el-input>
+                    <el-form-item label="签订时间" prop="HEZL_QDSJ">
+                        <el-input clearable v-model="ruleForm.HEZL_QDSJ"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="12">
-                    <el-form-item label="合同总金额" prop="CT_EMONEY">
-                        <el-input v-model="ruleForm.CT_EMONEY"></el-input>
+                    <el-form-item label="合同总金额" prop="HEZL_HTZJE">
+                        <el-input clearable v-model="ruleForm.HEZL_HTZJE"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
@@ -98,14 +140,97 @@
 
         <div class="upload-some">
             <div class="upload-header">原文信息</div>
-            
         </div>
+
+        <!-- 弹窗部分 -->
+        <el-dialog title="归档部门" :visible.sync="dialog_GDBM" width="30%">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialog_GDBM = false">取 消</el-button>
+                <el-button type="primary" @click="dialog_GDBM = false">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="责任者" :visible.sync="dialog_ZRZ" width="30%">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialog_ZRZ = false">取 消</el-button>
+                <el-button type="primary" @click="dialog_ZRZ = false">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="档案目录" :visible.sync="dialog_DNML" width="30%">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialog_DNML = false">取 消</el-button>
+                <el-button type="primary" @click="dialog_DNML = false">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
 import procesChildBarnav from "@/views/proces/proces-child-barnav";
 import rules from "@/utils/rules/index";
+// 年度
+const NDOPT = [
+    {
+        value: "选项1",
+        label: "2016"
+    },
+    {
+        value: "选项2",
+        label: "2017"
+    },
+    {
+        value: "选项3",
+        label: "2018"
+    },
+    {
+        value: "选项4",
+        label: "2019"
+    },
+    {
+        value: "选项5",
+        label: "2020"
+    }
+];
+// 保管期限
+const BGQXOPT = [
+    {
+        value: "选项1",
+        label: "10年"
+    },
+    {
+        value: "选项2",
+        label: "30年"
+    },
+    {
+        value: "选项3",
+        label: "永久"
+    }
+];
+// 密级
+const MKOPT = [
+    {
+        value: "选项1",
+        label: "普通"
+    },
+    {
+        value: "选项2",
+        label: "商业秘密"
+    },
+    {
+        value: "选项3",
+        label: "秘密"
+    },
+    {
+        value: "选项4",
+        label: "机密"
+    },
+    {
+        value: "选项5",
+        label: "绝密"
+    }
+];
 export default {
     name: "hetongzhulu",
     components: {
@@ -114,25 +239,39 @@ export default {
     data() {
         return {
             ruleForm: {
-                FONDSID: "", // 全宗号
-                FONDS_NAME: "", // 全宗名
-                DA_CODE: "", // 档号
-                DA_YEAR: "", // 年度
-                DEPT_NAME: "", // 归档部门
-                DA_TERM: "", // 保管期限
-                ARCH_SECRET: "", // 密级
-                IS_PUBLIC: "", // 是否公开
-                ARCH_TITLE: "", // 题名
-                CT_UNIT: "", // 合同相关方
-                CT_PROJ: "", // 所属项目
-                DUTY_NAME: "", // 责任者
-                ID_NAME: "", // 档案目录
-                BOX_NUM: "", // 盒号
-                CT_STIME: "", // 签订时间
-                CT_EMONEY: "" // 合同总金额
+                HEZL_QZH: "SCGF", // 全宗号
+                HEZL_QZM: "北京首创股份有限公司", // 全宗名
+                HEZL_DH: "", // 档号
+                HEZL_ND: "2020", // 年度
+                HEZL_GDBM: "", // 归档部门
+                HEZL_BGQX: "10年", // 保管期限
+                HEZL_MJ: "商业秘密", // 密级
+                HEZL_SFGK: 2, // 是否公开
+                HEZL_TM: "", // 题名
+                HEZL_HTXGF: "", // 合同相关方
+                HEZL_SSXM: "", // 所属项目
+                HEZL_ZRZ: "", // 责任者
+                HEZL_DNML: "", // 档案目录
+                HEZL_HH: "", // 盒号
+                HEZL_QDSJ: "", // 签订时间
+                HEZL_HTZJE: "" // 合同总金额
             },
-            rules
+            rules, // 验证规则
+            NDOPT, // 年度下拉
+            BGQXOPT, // 保管期限下拉
+            MKOPT, // 密级下拉
+            dialog_GDBM: false, // 归档部门弹窗
+            dialog_ZRZ: false, // 责任者弹窗
+            dialog_DNML: false // 档案目录弹窗
         };
+    },
+    methods: {
+        OPEGDBM() {
+            console.log(111);
+        },
+        OPENZRZ() {
+            console.log(222);
+        }
     }
 };
 </script>
@@ -155,6 +294,13 @@ export default {
     padding: 30px 20px 0;
     background: #fff;
     margin-bottom: 20px;
+}
+
+.ruleForm /deep/ .onColor input {
+    background-color: #f5f7fa;
+    border-color: #e4e7ed;
+    color: #c0c4cc;
+    cursor: pointer;
 }
 
 .upload-some {
--- new-oa/src/utils/rules/index.js
+++ new-oa/src/utils/rules/index.js
@@ -1,53 +1,53 @@
-const rules = {
-    // 合同著录
-    FONDS_ID: [
-        { required: true, message: "全宗号不能为空", trigger: "blur" }
-    ],
-    FONDS_NAME: [
-        { required: true, message: "全宗名不能为空", trigger: "blur" }
-    ],
-    DA_CODE: [
-        { required: true, message: "档号不能为空", trigger: "blur" }
-    ],
-    DA_YEAR: [
-        { required: true, message: "年度不能为空", trigger: "blur" }
-    ],
-    DEPT_NAME: [
-        { required: true, message: "归档部门不能为空", trigger: "blur" }
-    ],
-    DA_TERM: [
-        { required: true, message: "保管期限不能为空", trigger: "blur" }
-    ],
-    ARCH_SECRET: [
-        { required: true, message: "密级不能为空", trigger: "blur" }
-    ],
-    CT_STIME: [
-        { required: true, message: "是否公开不能为空", trigger: "blur" }
-    ],
-    ARCH_TITLE: [
-        { required: true, message: "题名不能为空", trigger: "blur" }
-    ],
-    CT_UNIT: [
-        { required: true, message: "合同相关方不能为空", trigger: "blur" }
-    ],
-    CT_PROJ: [
-        { required: true, message: "所属项目不能为空", trigger: "blur" }
-    ],
-    DUTY_NAME: [
-        { required: true, message: "责任者不能为空", trigger: "blur" }
-    ],
-    ID_NAME: [
-        { required: true, message: "档案目录不能为空", trigger: "blur" }
-    ],
-    BOX_NUM: [
-        { required: true, message: "盒号不能为空", trigger: "blur" }
-    ],
-    FONDS_ID: [
-        { required: true, message: "签订时间不能为空", trigger: "blur" }
-    ],
-    CT_EMONEY: [
-        { required: true, message: "合同总金额不能为空", trigger: "blur" }
-    ]
+export default {
+    rules: {
+        // 档案管理-合同著录
+        HEZL_QZH: [
+            { required: true, message: "全宗号不能为空", trigger: "blur" }
+        ],
+        HEZL_QZM: [
+            { required: true, message: "全宗名不能为空", trigger: "blur" }
+        ],
+        HEZL_DH: [
+            { message: "档号不能为空", trigger: "blur" }
+        ],
+        HEZL_ND: [
+            { required: true, message: "年度不能为空", trigger: "blur" }
+        ],
+        HEZL_GDBM: [
+            { message: "归档部门不能为空", trigger: "blur" }
+        ],
+        HEZL_BGQX: [
+            { required: true, message: "保管期限不能为空", trigger: "blur" }
+        ],
+        HEZL_MJ: [
+            { required: true, message: "密级不能为空", trigger: "blur" }
+        ],
+        HEZL_SFGK: [
+            { message: "是否公开不能为空", trigger: "blur" }
+        ],
+        HEZL_TM: [
+            { required: true, message: "题名不能为空", trigger: "blur" }
+        ],
+        HEZL_HTXGF: [
+            { message: "合同相关方不能为空", trigger: "blur" }
+        ],
+        HEZL_SSXM: [
+            { message: "所属项目不能为空", trigger: "blur" }
+        ],
+        HEZL_ZRZ: [
+            { message: "责任者不能为空", trigger: "blur" }
+        ],
+        HEZL_DNML: [
+            { required: true, message: "档案目录不能为空", trigger: "blur" }
+        ],
+        HEZL_HH: [
+            { message: "盒号不能为空", trigger: "blur" }
+        ],
+        HEZL_QDSJ: [
+            { message: "签订时间不能为空", trigger: "blur" }
+        ],
+        HEZL_HTZJE: [
+            { message: "合同总金额不能为空", trigger: "blur" }
+        ]
+    }
 }
-
-export default rules
