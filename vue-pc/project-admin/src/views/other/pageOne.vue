1b19ad9f9dfd78d5ff153cac7b1eeba517a0728a
chendongliang <13439594353@163.com>
2020-08-12 10:10:37 +0800

    加载组件

--- vue-pc/project-admin/src/views/other/pageOne.vue
+++ vue-pc/project-admin/src/views/other/pageOne.vue
@@ -1,13 +1,20 @@
 <template>
-    <div class="pageOne">pageOne</div>
+    <div class="pageOne">
+        pageOne
+        <common-loading></common-loading>
+    </div>
 </template>
 
 <script>
+import CommonLoading from "@/components/global/CommonLoading";
 export default {
     name: "PageOne",
+    components: {
+        CommonLoading,
+    },
     data() {
         return {};
-    }
+    },
 };
 </script>
 
